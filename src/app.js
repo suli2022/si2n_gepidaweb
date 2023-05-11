ac2ac7e85ac60b85c6e301f4a13d82023150bb6e
Nagy Janos <nagy@zold.lan>
2023-05-11 10:48:29 +0200

    Kész

---
+++ src/app.js
@@ -0,0 +1,58 @@
+/*
+* File: app.js
+* Author: Nagy János
+* Copyright: 2023, Nagy János
+* Group: Szoft I-2 N
+* Date: 2023-05-11
+* Github: https://github.com/janos/
+* Licenc: GNU GPL
+*/
+
+/* Kötések HTML elem és JavaScript változó */
+const doc = {
+    tbody: document.querySelector('#tbody')
+};
+/* Az alkalmazás állapota */
+const state = {
+    host: 'http://localhost:8000/',
+    bikes: []
+};
+/* Az oldal betöltésre mi történjen */
+window.addEventListener('load', () => {    
+    init();
+});
+/* Az alkalmazás előkészítése */
+function init() {
+    getBikes();
+}
+/* Elhozzuk az adatokat a REST API-tól */
+function getBikes() {    
+    let endpoint = 'bikes';
+    let url = state.host + endpoint;
+    fetch(url)
+    .then(response => response.json())
+    .then(result => {        
+        state.bikes = result;
+        render();
+    });
+}
+/* Megjeleníti az adatokat a weblapon táblázatban */
+function render() {
+    var rows = '';
+    /* A forEach() futtatható tömbökön, listákon */
+    state.bikes.forEach( bike => {
+        var row = `
+        <tr>
+            <td>${bike.id}</td>
+            <td>${bike.name}</td>
+            <td>${bike.wheel}</td>
+            <td>${bike.usage}</td>
+            <td>${bike.price}</td>
+        </tr>
+        `;
+        rows += row;        
+        console.log(bike.name)
+    })
+    doc.tbody.innerHTML = rows;
+}
+
